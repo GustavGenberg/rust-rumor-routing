63fb725a824f064bbc2df5ef59bd0ecac980b4c9
Gustav Genberg <gurra.genberg@gmail.com>
2023-05-26 01:05:17 +0200

    remove derive clone for message

--- src/messages/message.rs
+++ src/messages/message.rs
@@ -2,7 +2,6 @@ use crate::messages::agent_message::AgentMessage;
 use crate::messages::request_message::RequestMessage;
 use crate::messages::response_message::ResponseMessage;
 
-#[derive(Clone)]
 pub enum Message {
     AgentMessage(AgentMessage),
     RequestMessage(RequestMessage),
